d9c3b41ea2872aae88d4e638099da3c6e26f5154
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-07-21 21:25:14 -0700

    Set up configuration

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Router} from 'react-router-dom'
+
+
+ReactDOM.render(
+    <div>Hello, world!</div>,
+    document.getElementById('app') 
+  );
