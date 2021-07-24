988e468632b920891ab955fae1e4349b4ba9bed2
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-07-24 11:29:46 -0700

    Clean SingleBreed component

--- client/components/SingleBreed.js
+++ client/components/SingleBreed.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import React from "react";
 import axios from "axios";
 
@@ -11,28 +10,29 @@ export default class SingleBreed extends React.Component {
   }
 
   async componentDidMount() {
+    const copyPictures = [];
 
     for (let i = 0; i < 4; i++) {
       try {
         const breed = this.props.location.params.dog;
         const res = await axios.get(`https://dog.ceo/api/breed/${breed}/images/random`);
-
-        const copy = [...this.state.pictures];
-        copy.push(res.data.message)
-        this.setState({
-          pictures: copy
-        });
+        copyPictures.push(res.data.message);
       } catch (err) {
         console.error(err.message);
       }
     }
+
+    this.setState({
+      pictures: copyPictures
+    });
   }
 
   render() {
     const pictures = this.state.pictures;
     const breed = this.props.location.params.dog.slice(0, 1).toUpperCase() + this.props.location.params.dog.slice(1);
+
     return !pictures || pictures.length === 0 ? (
-      <h1>No Pictures Available</h1>
+      <h1>No Picture Available</h1>
     ) : (
       <div>
         <h1>{breed}</h1>
@@ -42,7 +42,7 @@ export default class SingleBreed extends React.Component {
               <div key={index}>
                 <ol>
                   <div>
-                  <img src={picture} alt="image" />
+                    <img src={picture} alt="image" />
                   </div>
                 </ol>
               </div>
