9f68635d53f162ac8d8c1276d706a67d5edb9f6e
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-07-25 20:33:34 -0700

    Implement local storage to to persisting state between page reloads

--- client/components/SingleBreed.js
+++ client/components/SingleBreed.js
@@ -2,35 +2,59 @@ import React from "react";
 import axios from "axios";
 
 export default class SingleBreed extends React.Component {
+
   constructor(props) {
     super(props);
+    let pictures = [];
+
+    if (JSON.parse(window.localStorage.getItem('pictures'))) {
+      pictures = JSON.parse(window.localStorage.getItem('pictures'))
+    }
+
+    let breed = '';
+    if (JSON.parse(window.localStorage.getItem('breed'))) {
+      breed = JSON.parse(window.localStorage.getItem('breed'));
+    }
+
+
     this.state = {
-      pictures: []
+      pictures: pictures,
+      breed: breed
     }
   }
 
   async componentDidMount() {
-    const copyPictures = [];
+    if (this.props.location.params !== undefined) {
+      const copyPictures = [];
 
-    for (let i = 0; i < 4; i++) {
-      try {
-        const breed = this.props.location.params.dog;
-        const res = await axios.get(`https://dog.ceo/api/breed/${breed}/images/random`);
-        copyPictures.push(res.data.message);
-      } catch (err) {
-        console.error(err.message);
-      }
+      for (let i = 0; i < 4; i++) {
+        try {
+
+          const breed = this.props.location.params.dog;
+
+          window.localStorage.setItem('breed', JSON.stringify(breed));
+
+          this.setState({
+            breed: breed
+          })
+          const res = await axios.get(`https://dog.ceo/api/breed/${breed}/images/random`);
+          copyPictures.push(res.data.message);
+        } catch (err) {
+          console.error(err.message);
+        }
+        window.localStorage.setItem('pictures', JSON.stringify(copyPictures));
+
+        this.setState({
+          pictures: copyPictures
+        })
+      } 
     }
-
-    this.setState({
-      pictures: copyPictures
-    });
   }
 
   render() {
+    const breed = this.state.breed.slice(0, 1).toUpperCase() + this.state.breed.slice(1);
     const pictures = this.state.pictures;
-    const breed = this.props.location.params.dog.slice(0, 1).toUpperCase() + this.props.location.params.dog.slice(1);
-
+    
     return !pictures || pictures.length === 0 ? (
       <h1>Loading...</h1>
     ) : (
